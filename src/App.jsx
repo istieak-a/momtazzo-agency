dd239776649d5d6853ea3d5dddf3fda651f7140c
Istieak Ahmed <istieak.ahmed@yahoo.com>
2024-05-24 02:15:55 +0600

    chore: Add Footer component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Homepage from "./Components/Homepage/Homepage";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
+      <Footer />
 
     </BrowserRouter>
   );
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import logoWhite from "../../assets/logo-inverse-retina.png";
+import "./Footer.css";
+const Footer = () => {
+  const socialData = [
+    {
+      title: "Facebook",
+      icon: <FaFacebookF />,
+      link: "#",
+    },
+    {
+      title: "Twitter",
+      icon: <FaTwitter />,
+      link: "#",
+    },
+    {
+      title: "Instagram",
+      icon: <FaInstagram />,
+      link: "#",
+    },
+    {
+      title: "Linkedin",
+      icon: <FaLinkedinIn />,
+      link: "#",
+    },
+  ];
+  const navItems = [
+    {
+      title: "Home",
+      url: "/",
+    },
+    {
+      title: "Services Vertical",
+      url: "/services",
+      submenu: [
+        {
+          title: "Offshore Staffing",
+          url: "/offshore-staffing",
+        },
+        {
+          title: "B2B Lead Generation",
+          url: "/b2b-lead-generation",
+        },
+        {
+          title: "RPO",
+          url: "/rpo",
+        },
+        {
+          title: "Digital Marketing Service",
+          url: "/digital-marketing-service",
+        },
+        {
+          title: "Call Center",
+          url: "/call-center",
+        },
+      ],
+    },
+    {
+      title: "About Us",
+      url: "/about",
+    },
+    {
+      title: "Career",
+      url: "/career",
+    },
+  ];
+  return (
+    <div>
+      <div className="flex bg-[#ecf4f5]">
+        {socialData.map((social, index) => {
+          return (
+            <a
+              key={index}
+              href={social.link}
+              target="_blank"
+              className="flex items-center justify-center gap-2 w-[25%] font-outfit text-[14px] py-3 md:py-8 link-border font-[500]"
+            >
+              {social.icon}
+              {social.title}
+            </a>
+          );
+        })}
+      </div>
+      <div className="bg-[#07131d] py-20">
+        <div>
+          <img src={logoWhite} alt="" />
+        </div>
+        <div className="text-white">
+          <h1 className="font-outfit text-[18px] font-[500]">Office</h1>
+          <p className="font-dmsans text-[16px]">
+            Germany — <br />
+            785 15h Street, Office 478 <br />
+            Berlin, De 81566
+          </p>
+          <a href="mailto:info@gmail.com" className="font-dmsans text-[16px]">
+            info@gmail.com
+          </a>
+          <a href="#" className="font-dmsans text-[18px]">
+            +18408412569
+          </a>
+        </div>
+        <div>
+          <h1>Links</h1>
+          <a href=""></a>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
