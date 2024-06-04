a41d45289c51e1070273d37af32d5ddb0e9155de
Istieak Ahmed <istieak.ahmed@yahoo.com>
2024-06-05 00:29:30 +0600

    chore: Update FilipinoBenifits component to use Indian instead of Filipino in title

--- src/Components/Sections/FilipinoBenifits/FilipinoBefinits.jsx
+++ src/Components/Sections/FilipinoBenifits/FilipinoBefinits.jsx
@@ -26,12 +26,12 @@ const FilipinoBefinits = ({title}) => {
             ico: 'https://www.microsourcing.com/hubfs/MicroSourcing/Build%20Your%20Team/template-assets/icons/Access%2024_7%20support.svg', 
             title: 'Access 24/7 support',
             desc: 'Get the support you need, when you need it.', 
-            link: '/why-outsource/why-microsourcing'
+            link: '/why-outsource/why-montazzo'
         }
     ]
     return (
         <div className='flex px-5 md:px-16 items-center flex-col md:flex-row bg-[#f4fafc] py-10 gap-10'>
-            <h1 className='font-outfit text-5xl font-semibold text-center'>What are the benifits of Filipino {title} outsourcing?</h1>
+            <h1 className='font-outfit text-5xl font-semibold text-center'>What are the benifits of Indian {title} outsourcing?</h1>
             <div className='grid grid-cols-2 gap-5'>
                 {cardData.map((card, i) => (
                     <Card key={i} title={card.title} ico={card.ico} desc={card.desc} link={card.link} />
