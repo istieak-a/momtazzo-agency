0cc84f7d5b3bbe6ee7270c305314884f447b2e36
Istieak Ahmed <istieak.ahmed@yahoo.com>
2024-06-05 23:12:12 +0600

    chore: Update HeroBanner component styles for improved responsiveness and add background image

--- src/Components/Sections/HeroBanner/HeroBanner.jsx
+++ src/Components/Sections/HeroBanner/HeroBanner.jsx
@@ -7,7 +7,7 @@ const HeroBanner = ({ title, title2, desc, img, imgRes }) => {
 
   return (
     <div
-      className="py-20 text-white px-5 md:px-16"
+      className="py-20 md:py-30 text-white px-5 md:px-16 bg-no-repeat bg-cover"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className=" max-w-7xl mx-auto">
@@ -17,7 +17,7 @@ const HeroBanner = ({ title, title2, desc, img, imgRes }) => {
         {title2 && (
           <h2 className="text-center md:text-left font-outfit text-5xl">{title2}</h2>
         )}
-        <p className="pt-7 text-center text-lg md:text-left ">
+        <p className="pt-7 text-center text-lg md:text-left  w-1/2">
           {desc}
         </p>
       </div>
