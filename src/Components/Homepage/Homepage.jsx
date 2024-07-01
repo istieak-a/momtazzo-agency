b8bda5147da2421a85bfb90d466c9209fd49b204
Istieak Ahmed <istieak.ahmed@yahoo.com>
2024-07-01 01:35:59 -0700

    chore: Update HowItWorks component with new content and styles

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -23,7 +23,9 @@ import BuildTeam from "../Sections/BuildTeam/BuildTeam";
 import Accordion from "../Accordian/Accordian";
 import checkIco from "../../assets/icons/pngegg.png";
 import BlogCard from "../BlogCard/BlogCard";
-
+import faqbg from "../../assets/faqbg.png";
+import Faq from "react-faq-component";
+import "../HowItWorks/Howitworks.css";
 const Homepage = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
   const partners = [
@@ -234,8 +236,60 @@ const Homepage = () => {
       img: "https://influx.com/blog/uploads/case-studies-2.png",
       link: "case-study-3",
     },
-
   ];
+
+  const data = {
+    rows: [
+      {
+        title: "1. Deep dive into your goals and team structure.",
+        content:
+          "We take the time to understand your unique needs and goals. Our talent becomes an extension of your team, supporting your customer experience initiatives within your strategic direction, timelines and budgets.",
+      },
+      {
+        title: "2. We design a customized solution for you.",
+        content:
+          "Within 1 week, we'll design a customized solution tailored to your unique needs. This includes selecting the right talent, customizing training and onboarding, and integrating with your existing processes - optimized for you.",
+      },
+      {
+        title: "3. Review and assemble your dream team.",
+        content:
+          "Let us handle the screening and recruitment so you can build your dream team. We identify candidates who match your precise needs, coordinate interviews, collect your feedback, and refine the applicant pool until you land on just the right hires.",
+      },
+      {
+        title: "4. We'll implement a dedicated training program.",
+        content:
+          "Our training, onboarding and assessments integrate with your processes while optimizing for scale and cultural nuance. We develop programs tailored to complement your approach.",
+      },
+      {
+        title: "5. Go Live & schedule check-ins!",
+        content:
+          "Curtains Up - Your dedicated team is now fully operational. We will set up regular check-ins that work with your schedule, not ours. This ensures you have ongoing visibility into performance and can provide timely feedback based on your availability.",
+      },
+    ],
+  };
+  const styles = {
+    bgColor: "transparent",
+    titleTextColor: "#333",
+    titleTextSize: "36px",
+    rowTitleColor: "#444",
+    rowTitleTextSize: "24px",
+    rowContentColor: "#666",
+    rowContentTextSize: "18px",
+    rowContentPaddingTop: "15px",
+    rowContentPaddingBottom: "15px",
+    rowContentPaddingLeft: "20px",
+    rowContentPaddingRight: "20px",
+    arrowColor: "#333",
+    transitionDuration: "0.5s",
+    timingFunc: "ease",
+  };
+
+  const config = {
+    animate: true,
+    arrowIcon: "▼",
+    expandIcon: "+",
+    collapseIcon: "-",
+  };
 
   return (
     <div>
@@ -337,6 +391,28 @@ const Homepage = () => {
         </div>
       </div>
 
+      <div className="bg-[#fff9e6]">
+        <div className="mx-auto flex max-w-7xl flex-col items-center gap-8 px-5 py-10 md:px-28 md:py-20">
+          <h1 className="font-outfit text-5xl font-normal">
+            How does it work?
+          </h1>
+          <p className="w-[70%] text-center text-lg md:w-1/2">
+            We source, train, and assemble your new team in as little as 2
+            weeks. Once you go live, we continuously work to ensure you hit
+            KPIs.
+          </p>
+        </div>
+
+        <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-10 px-5 py-10 md:flex-row md:px-28">
+          <div className="faq-container md:w-[80%]">
+            <Faq data={data} styles={styles} config={config} />
+          </div>
+          <div>
+            <img src={faqbg} alt="" />
+          </div>
+        </div>
+      </div>
+
       <div className="mx-auto flex max-w-7xl flex-col items-center gap-5 px-5 py-10 md:px-16 md:py-20">
         <h1 className="font-outfit text-5xl">More solutions</h1>
         <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-4">
@@ -361,19 +437,19 @@ const Homepage = () => {
         </div>
       </div>
 
-      <div className="mx-auto flex max-w-7xl flex-col items-center gap-5 px-5 py-10 md:px-16 md:py-20 bg-[#f4fafc]">
+      <div className="mx-auto flex max-w-7xl flex-col items-center gap-5 bg-[#f4fafc] px-5 py-10 md:px-16 md:py-20">
         <h1 className="font-outfit text-5xl">Case Studies</h1>
         <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-5 py-10 md:grid-cols-3 md:px-16">
-        {caseStudies.map((caseStudy, index) => (
-          <BlogCard
-            key={index}
-            title={caseStudy.title}
-            desc={caseStudy.desc}
-            img={caseStudy.img}
-            link={caseStudy.link}
-          />
-        ))}
-      </div>
+          {caseStudies.map((caseStudy, index) => (
+            <BlogCard
+              key={index}
+              title={caseStudy.title}
+              desc={caseStudy.desc}
+              img={caseStudy.img}
+              link={caseStudy.link}
+            />
+          ))}
+        </div>
       </div>
 
       {/* <Accordion /> */}
--- src/Components/HowItWorks/HowItWorks.jsx
+++ src/Components/HowItWorks/HowItWorks.jsx
@@ -1,35 +1,41 @@
 import React from "react";
 import faqbg from "../../assets/faqbg.png";
 import Faq from "react-faq-component";
-import './Howitworks.css'
+import "./Howitworks.css";
+import Reviews from "../Reviews/Reviews";
 
 const HowItWorks = () => {
   const data = {
     rows: [
       {
         title: "1. Deep dive into your goals and team structure.",
-        content: "We take the time to understand your unique needs and goals. Our talent becomes an extension of your team, supporting your customer experience initiatives within your strategic direction, timelines and budgets."
-    },
-    {
+        content:
+          "We take the time to understand your unique needs and goals. Our talent becomes an extension of your team, supporting your customer experience initiatives within your strategic direction, timelines and budgets.",
+      },
+      {
         title: "2. We design a customized solution for you.",
-        content: "Within 1 week, we'll design a customized solution tailored to your unique needs. This includes selecting the right talent, customizing training and onboarding, and integrating with your existing processes - optimized for you."
-    },
-    {
+        content:
+          "Within 1 week, we'll design a customized solution tailored to your unique needs. This includes selecting the right talent, customizing training and onboarding, and integrating with your existing processes - optimized for you.",
+      },
+      {
         title: "3. Review and assemble your dream team.",
-        content: "Let us handle the screening and recruitment so you can build your dream team. We identify candidates who match your precise needs, coordinate interviews, collect your feedback, and refine the applicant pool until you land on just the right hires."
-    },
-    {
+        content:
+          "Let us handle the screening and recruitment so you can build your dream team. We identify candidates who match your precise needs, coordinate interviews, collect your feedback, and refine the applicant pool until you land on just the right hires.",
+      },
+      {
         title: "4. We'll implement a dedicated training program.",
-        content: "Our training, onboarding and assessments integrate with your processes while optimizing for scale and cultural nuance. We develop programs tailored to complement your approach."
-    },
-    {
+        content:
+          "Our training, onboarding and assessments integrate with your processes while optimizing for scale and cultural nuance. We develop programs tailored to complement your approach.",
+      },
+      {
         title: "5. Go Live & schedule check-ins!",
-        content: "Curtains Up - Your dedicated team is now fully operational. We will set up regular check-ins that work with your schedule, not ours. This ensures you have ongoing visibility into performance and can provide timely feedback based on your availability."
-    }
+        content:
+          "Curtains Up - Your dedicated team is now fully operational. We will set up regular check-ins that work with your schedule, not ours. This ensures you have ongoing visibility into performance and can provide timely feedback based on your availability.",
+      },
     ],
   };
   const styles = {
-    bgColor: "transparent",  
+    bgColor: "transparent",
     titleTextColor: "#333",
     titleTextSize: "36px",
     rowTitleColor: "#444",
@@ -43,38 +49,41 @@ const HowItWorks = () => {
     arrowColor: "#333",
     transitionDuration: "0.5s",
     timingFunc: "ease",
-};
+  };
 
-const config = {
+  const config = {
     animate: true,
     arrowIcon: "▼",
     expandIcon: "+",
     collapseIcon: "-",
-};
+  };
 
   return (
     <div className="bg-[#fff9e6]">
-    <div className="flex flex-col items-center gap-8 px-5 py-10 md:px-28 md:py-20">
-      <h1 className="font-outfit text-5xl font-normal">How does it work?</h1>
-      <p className="w-1/2 text-center text-lg">
-        We source, train, and assemble your new team in as little as 2 weeks.
-        Once you go live, we continuously work to ensure you hit KPIs.
-      </p>
-    </div>
+      <div className="mx-auto flex max-w-7xl flex-col items-center gap-8 px-5 py-10 md:px-28 md:py-20">
+        <h1 className="font-outfit text-5xl font-normal">How does it work?</h1>
+        <p className="w-[70%] text-center text-lg md:w-1/2">
+          We source, train, and assemble your new team in as little as 2 weeks.
+          Once you go live, we continuously work to ensure you hit KPIs.
+        </p>
+      </div>
 
-    <div className="flex items-center justify-between gap-10 px-5 md:px-28 py-10">
-      <div className="w-[70%] faq-container">
-        <Faq
-          data={data}
-          styles={styles}
-          config={config}
-        />
+      <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-10 px-5 py-10 md:flex-row md:px-28">
+        <div className="faq-container md:w-[80%]">
+          <Faq data={data} styles={styles} config={config} />
+        </div>
+        <div>
+          <img src={faqbg} alt="" />
+        </div>
       </div>
-      <div>
-        <img src={faqbg} alt="" />
+
+      <div className="mx-auto max-w-7xl px-5 py-20">
+        <h2 className="pb-10 text-center font-outfit text-4xl font-bold">
+          What Our Clients Are Saying
+        </h2>
+        <Reviews />
       </div>
     </div>
-  </div>
   );
 };
 
