ccba3dc3d388b5647a269a97c0ddf2f0cc4404ad
Istieak Ahmed <istieak.ahmed@yahoo.com>
2024-07-05 01:04:58 +0600

    feat: Improve JobCard component link styling
    Update the link styling in the JobCard component to enhance the user experience. The background color, hover effects, and text color have been adjusted to provide better visual feedback to users when interacting with the links.

--- src/Components/JobCard/JobCard.jsx
+++ src/Components/JobCard/JobCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const JobCard = ({ title, link }) => {
   return (
-    <Link to={link} className='bg-[#f4fafc] hover:bg-[#425968] rounded-lg hover:shadow-md duration-300 text-[#3e4d5d] hover:text-[#ffffff]'>
+    <Link to={link} className='bg-[#f4fafc] hover:bg-[#425968] rounded-lg hover:shadow-xl duration-300 text-[#3e4d5d] hover:text-[#ffffff]'>
         <h1 className='text-lg px-5 py-4 font-semibold hover:underline'>{title}</h1>
     </Link>
   )
